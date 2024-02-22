4a55a25cbd0ac384469b8794638248abe655c610
omer12344 <130368849+omer12344@users.noreply.github.com>
2024-02-22 13:16:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+from ScanLan import ScanLanSession
+from Sniff import SniffSession
+email_receiver = str(input("what email you want to get updates to?"))
 
+scan_lan_session = ScanLanSession(email_receiver)
+sniff_session = SniffSession(1000, scan_lan_session.local_ips_objects, scan_lan_session.local_ips, email_receiver)
